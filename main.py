a92b605b7adb6492b3802b55c82e4e5fd2af5d94
Rafael Libel <rafael.rl1809@gmail.com>
2026-05-13 09:41:19 -0300

    Ajuste de porta e Procfile

--- main.py
+++ main.py
@@ -79,3 +79,7 @@ def serve_frontend():
         return {"erro": "Arquivo index.html não encontrado na pasta static!"}
     
     return FileResponse("static/index.html")
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
